91b8737287da763952dbd99d23ad7d3669793294
Wayne Gong (minbgong) <minbgong@cisco.com>
2019-02-22 14:58:35 +0800

    Update UI code Changes to be committed: 	modified:   DocumentTranslator/DocumentTranslator.pro.user 	modified:   DocumentTranslator/smarttranslator.h 	modified:   DocumentTranslator/smarttranslator.ui

--- DocumentTranslator/smarttranslator.h
+++ DocumentTranslator/smarttranslator.h
@@ -2,6 +2,7 @@
 #define SMARTTRANSLATOR_H
 
 #include <QMainWindow>
+#include <QString>
 
 namespace Ui {
 class SmartTranslator;
@@ -14,6 +15,27 @@ class SmartTranslator : public QMainWindow
 public:
     explicit SmartTranslator(QWidget *parent = nullptr);
     ~SmartTranslator();
+
+//    void loadFile(const QString &fileName);
+
+//private slots:
+//    void newFile();
+//    void open();
+//    void save();
+//    void saveAs();
+//    void about();
+
+//    void documentWasModified();
+
+//private:
+//    void createActions();
+//    void createStatusBar();
+//    void readSettings();
+//    void writeSettings();
+//    bool maybeSave();
+//    bool saveFile(const QString &fileName);
+//    void setCurrentFile(const QString &fiileName);
+//    QString strippedName(const QString &fullFileName);
 
 private:
     Ui::SmartTranslator *ui;
